32e0352a672aaf274a962e8b5946c5951e6beb29
Roman Zaytsev <roman.zaytsev@colorado.edu>
2018-04-03 22:32:05 -0600

    Combined SQL files into 1
    Everything works except final create Roster command.

--- Project Deliverables/Back End/COMBINED_SQL_FILE.sql
+++ Project Deliverables/Back End/COMBINED_SQL_FILE.sql
@@ -1,10 +1,88 @@
---open terminal in same directory, run command 'mysql -u root -p < project.sql
+#open terminal in same directory, run command mysql -u root -p < project.sql
 CREATE DATABASE project;
 USE project;
 
-CREATE TABLE Teacher (
-	TeacherID int NOT NULL,
-	LastName char(20), 
-	FirstName char(20),
-	PRIMARY KEY (TeacherID)
+CREATE TABLE Student (
+	StudentID 	INT(9) 	NOT NULL,
+	LastName 	CHAR(20) 	NOT NULL,
+	FirstName 	CHAR(20) 	NOT NULL,
+	Email 		CHAR(20) 	NOT NULL,
+	Passwrd 	CHAR(10) 	NOT NULL,
+    
+	PRIMARY KEY (StudentID)
+);
+
+CREATE TABLE  Teacher (
+	TeacherID	INT(9)		NOT NULL,
+	LastName	CHAR(20)	NOT NULL,
+	FirstName	CHAR(20)   	NOT NULL,
+    
+	PRIMARY KEY (TeacherID)  
+);
+
+CREATE TABLE TACA (
+	AssistantID INT(9) 		NOT NULL,
+	LastName 	CHAR(20)	NOT NULL,
+	FirstName 	CHAR(20) 	NOT NULL,
+	Email 		CHAR(20) 	NULL,
+    
+	PRIMARY KEY (AssistantID)
+);
+
+CREATE TABLE StudentSchedule (
+	StudentID	INT(9)   	NOT NULL,
+	ClassID   	INT(7)    	NOT NULL,
+    
+	PRIMARY KEY (StudentID, ClassID)  
+);
+
+
+CREATE TABLE Class (
+	ClassID 	INT(7) 		NOT NULL,
+	TeacherID 	INT(9) 		NOT NULL,
+	LastName 	CHAR(20) 	NOT NULL,
+	FirstName 	CHAR(20) 	NOT NULL,
+	Department 	INT(20) 	NOT NULL,
+	DayOfWeek 	CHAR(10) 	NOT NULL,
+	FromTime 	TIME 		NOT NULL,
+	ToTime 		TIME 		NOT NULL,
+	Room 		INT(5) 		NOT NULL,
+	Building 	CHAR(20) 	NOT NULL,
+    
+	PRIMARY KEY (ClassID),
+    
+	FOREIGN KEY (TeacherID)
+		REFERENCES Teacher(TeacherID)
+);
+
+CREATE TABLE TACASchedule (
+	ScheduleID 	INT(5) 		NOT NULL 		AUTO_INCREMENT,
+	AssistantID INT(9) 		NOT NULL,
+	ClassID 	INT(7) 		NOT NULL,
+	DayOfWeek 	CHAR(10) 	NOT NULL,
+	FromTime 	TIME 		NOT NULL,
+	ToTime 		TIME 		NOT NULL,
+	Room 		INT(5) 		DEFAULT NULL,
+	Building 	CHAR(10) 	DEFAULT NULL,
+    
+	PRIMARY KEY(ScheduleID),
+    
+	FOREIGN KEY (AssistantID)
+		REFERENCES TACA(AssistantID),
+        
+	FOREIGN KEY (ClassID)
+		REFERENCES Class(ClassID)
+);
+
+CREATE TABLE Roster (
+	StudentID 	INT(5) 		NOT NULL,
+	ClassID 	INT(7) 		NOT NULL,
+    
+	PRIMARY KEY (StudentID, ClassID),
+    
+	FOREIGN KEY (StudentID)
+		REFERENCES Student(StudentID),
+        
+	FOREIGN KEY (ClassID)
+		REFERENCES Class(StudentID)
 );
