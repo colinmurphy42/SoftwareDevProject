4ae9796300f30520d345946ac4f46b601575c410
Colin Murphy <36647057+colinmurphy42@users.noreply.github.com>
2018-04-23 18:54:10 -0600

    Potential php file

---
+++ Project Deliverables/Back End/signup.php
@@ -0,0 +1,10 @@
+<?
+
+$connection = pg_connect("dbname= d30ja9oeokuudf user= oxajqwvmxwcygq host= ec2-54-221-192-231.compute-1.amazonaws.com password= 88649658013b5e0cbdf162b20cc5c46160b147eb6837d4bcc36a79b3fe107cbd");
+if (!$connection) {
+    print("Connection Failed.");
+    exit;
+  }
+$myresult = pg_exec("INSERT INTO student VALUES ('$POST_[studi], $POST_[firstn], $POST_[lastn], $POST_[password])";
+
+?>
